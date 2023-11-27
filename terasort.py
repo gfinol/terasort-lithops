3faf3416c400af2664b35c1ebd97d252b5a9c367
Gerard Finol <gerard.finol@urv.cat>
2023-11-27 18:22:07 +0100

    Default runtime memory set to 1769MB => 1 vCPU

--- terasort.py
+++ terasort.py
@@ -8,7 +8,7 @@ from terasort_faas.terasort import run_terasort
 @click.option("--map_parallelism", type=int, help="Number of map partitions")
 @click.option("--runtime_name", type=str, help="Docker runtime to use")
 @click.option("--reduce_parallelism", type=int, default=None, help="Number of reduce partitions")
-@click.option("--runtime_memory", type=int, default=1792, help="Default runtime memory per cloud function")
+@click.option("--runtime_memory", type=int, default=1769, help="Default runtime memory per cloud function")
 @click.option("--warm_up", type=bool, default=False, help="Warm up cloud functions before execution (recommended at the very first execution of a set)")
 def run(bucket, key, map_parallelism, runtime_name, reduce_parallelism, runtime_memory, warm_up):
 
